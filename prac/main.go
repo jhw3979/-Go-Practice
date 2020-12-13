13eacc0b491aa1102e946837236d48cf49a964ec
진현우 <49802982+jhw3979@users.noreply.github.com>
2020-12-13 22:20:28 +0900

    go practice
    시험기간

---
+++ prac/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var A string
+
+	A = "helloworld"
+
+	if A == "helloworld" {
+		fmt.Println("helloworld")
+	} else {
+		fmt.Println("hi")
+	}
+}
