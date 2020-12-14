9217fb5e875b6211d2c95e11fb681774acd48dd1
진현우 <49802982+jhw3979@users.noreply.github.com>
2020-12-14 23:50:45 +0900

    공부중

--- prac/main.go
+++ prac/main.go
@@ -5,13 +5,15 @@ import (
 )
 
 func main() {
-	var A string
-
-	A = "helloworld"
+	var A string = "helloworld"
+	var B string = "안녕"
 
 	if A == "helloworld" {
 		fmt.Println("helloworld")
 	} else {
 		fmt.Println("hi")
 	}
+
+	fmt.Println(len(A))
+	fmt.Println(len(B))
 }
